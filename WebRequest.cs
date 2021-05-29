0196b8c1f4dfd23f3260726f91a4f066b27ab385
Ege Bilecen <egebilecen@icloud.com>
2021-05-29 11:22:46 +0300

    Add files via upload

---
+++ WebRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+
+namespace EB_Utility
+{
+    public static class WebRequest
+    {
+        public static void DownloadImage(string imageUrl, string filename, ImageFormat format)
+        {    
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(imageUrl);
+            Bitmap bitmap;  bitmap = new Bitmap(stream);
+
+            if (bitmap != null)
+            {
+                bitmap.Save(filename, format);
+            }
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+        }
+    }
+}
