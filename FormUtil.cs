e44fab1aad8e4fd9ba6ced5b20854956c5d5b4c3
Ege Bilecen <egebilecen@icloud.com>
2022-07-25 08:07:57 +0300

    Add files via upload

---
+++ FormUtil.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Forms;
+
+namespace EB_Utility
+{
+    public static class FormUtil
+    {
+        private static Timer labelTimer = new Timer();
+
+        public static void UpdateProgressBar(ProgressBar progressBar, int value)
+        {
+            bool maxUpdated = false;
+
+            if(value + 1 > progressBar.Maximum)
+            {
+                progressBar.Maximum++;
+                maxUpdated = true;
+            }
+
+            progressBar.Value = value + 1;
+            progressBar.Value--;
+
+            if(maxUpdated) progressBar.Maximum--;
+        }
+
+        public static void ResetProgressBar(ProgressBar progressBar, int maxValue)
+        {
+            progressBar.Value   = 0;
+            progressBar.Maximum = maxValue;
+        }
+        
+        public static void DisplayHidingText(Label label, string text, int durationMS=0)
+        {
+            label.Text = text;
+            label.Show();
+
+            if(durationMS != 0)
+            {
+                labelTimer.Stop();
+
+                labelTimer.Interval = durationMS;
+                labelTimer.Tick += (s, e) => { 
+                    label.Hide();
+                    labelTimer.Stop();
+                };
+
+                labelTimer.Start();
+            }
+        }
+    }
+}
