ec97fd9ce51cfa99897ba4ae7539004585ee674f
Ege Bilecen <egebilecen@icloud.com>
2022-07-25 13:10:28 +0300

    Update SMTPMailer.cs

--- SMTPMailer.cs
+++ SMTPMailer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 public class SMTPMailer
 {
@@ -11,6 +12,8 @@ public class SMTPMailer
 
     public string fromAddr = null;
     public string fromName = null;
+
+    public SmtpClient smtpClient { get; private set; } = null;
 
     public SMTPMailer(string smtpHost, int smtpPort, string smtpUsername, string smtpPassword)
     {
@@ -40,9 +43,16 @@ public class SMTPMailer
         return mailMessage;
     }
 
+    public MailMessage CreateMailMessage(string toAddr, string subject, string body, bool isHTML=true)
+    {
+        MailMessage mailMessage = CreateMailMessage(subject, body, isHTML);
+        mailMessage.To.Add(toAddr);
+        return mailMessage;
+    }
+
     public bool SendMail(MailMessage mailMessage)
     {
-        using(var smtpClient = new SmtpClient(smtpHost, smtpPort))
+        using(smtpClient = new SmtpClient(smtpHost, smtpPort))
         {
             smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
             smtpClient.EnableSsl   = true;
@@ -60,16 +70,28 @@ public class SMTPMailer
             return false;
         }
     }
-
-    public MailMessage CreateMailMessage(string toAddr, string subject, string body, bool isHTML=true)
+    
+    public async Task SendMailAsync(MailMessage mailMessage, Action<Exception> callback=null)
     {
-        MailMessage mailMessage = CreateMailMessage(subject, body, isHTML);
-        mailMessage.To.Add(toAddr);
-        return mailMessage;
+        using(smtpClient = new SmtpClient(smtpHost, smtpPort))
+        {
+            smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+            smtpClient.EnableSsl   = true;
+
+            try
+            {
+                smtpClient.SendCompleted += (s, e) => callback(e.Error);
+                await smtpClient.SendMailAsync(mailMessage);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception message: " + ex.Message);
+            }
+        }
     }
 
     public bool Send(string toAddr, string subject, string body, bool isHTML=true)
-    { 
+    {
         PreCheck();
 
         MailMessage mail = CreateMailMessage(subject, body, isHTML);
@@ -86,5 +108,25 @@ public class SMTPMailer
         mail.To.Add(string.Join(",", toAddrList));
 
         return SendMail(mail);
+    }
+
+    public async Task SendAsync(string toAddr, string subject, string body, bool isHTML=true)
+    {
+        PreCheck();
+
+        MailMessage mail = CreateMailMessage(subject, body, isHTML);
+        mail.To.Add(new MailAddress(toAddr));
+
+        await SendMailAsync(mail);
+    }
+
+    public async Task SendAsync(string[] toAddrList, string subject, string body, bool isHTML=true)
+    {
+        PreCheck();
+
+        MailMessage mail = CreateMailMessage(subject, body, isHTML);
+        mail.To.Add(string.Join(",", toAddrList));
+
+        await SendMailAsync(mail);
     }
 }
