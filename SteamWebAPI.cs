a18c091d6c2ee28038e87b7b5ac9ae80be6e0c73
Ege Bilecen <egebilecen@icloud.com>
2022-08-17 07:08:13 +0300

    Update SteamWebAPI.cs

--- SteamWebAPI.cs
+++ SteamWebAPI.cs
@@ -6,74 +6,77 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using EB_Utility;
 
-public static class SteamWebAPI
+namespace EB_Utility
 {
-    public static class Model
+    public static class SteamWebAPI
     {
-        public class Tag
+        public static class Model
         {
-            [JsonProperty("Tag")]
-            public string Name;
-        }
-
-        public class WorkshopItemDetails
-        {
-            public string PublishedFileId;
-            public int    Result;
-            public string Creator;
-            public int    CreatorAppId;
-            public int    ConsumerAppId;
-            public string FileName;
-            public int    FileSize;
-            public string FileURL;
-            public string HContentFile;
-            public string PreviewURL;
-            public string HContentPreview;
-            public string Title;
-            public string Description;
-            public int    TimeCreated;
-            public int    TimeUpdated;
-            public int    Visibility;
-            public int    Banned;
-            public string BanReason;
-            public int    Subscriptions;
-            public int    Favorited;
-            public int    LifetimeSubscriptions;
-            public int    LifetimeFavorited;
-            public List<Tag> Tags;
-        }
-    }
-
-    private static HttpClient httpClient = WebRequest.CreateHTTPClient();
-    private static string     baseAPIURL = "https://api.steampowered.com/";
-
-    public static async Task GetWorkshopItemDetails(string[] idList)
-    {
-        string apiModule = "ISteamRemoteStorage/GetPublishedFileDetails/v1/";
-
-        var parameters = new List<KeyValuePair<string, string>>
-        {
-            new KeyValuePair<string, string>("itemCount", idList.Length.ToString())
-        };
-
-        for(int i=0; i < idList.Length; i++)
-        {
-            var id = idList[i];
-            parameters.Add(new KeyValuePair<string, string>("publishedfileids["+i.ToString()+"]", id.ToString()));
-        }
-
-        string res = await WebRequest.PostAsync(httpClient, baseAPIURL + apiModule, parameters);
-
-        JToken jsonData = JToken.Parse(res)["response"];
-
-        if((int)jsonData["result"]      == 1
-        && (int)jsonData["resultcount"] == idList.Length)
-        {
-            JArray workshopItems = (JArray)jsonData["publishedfiledetails"];
-
-            foreach(JToken elem in workshopItems)
+            public class Tag
             {
-                Model.WorkshopItemDetails itemDetails = elem.ToObject<Model.WorkshopItemDetails>();
+                [JsonProperty("Tag")]
+                public string Name;
+            }
+
+            public class WorkshopItemDetails
+            {
+                public string PublishedFileId;
+                public int    Result;
+                public string Creator;
+                public int    CreatorAppId;
+                public int    ConsumerAppId;
+                public string FileName;
+                public int    FileSize;
+                public string FileURL;
+                public string HContentFile;
+                public string PreviewURL;
+                public string HContentPreview;
+                public string Title;
+                public string Description;
+                public int    TimeCreated;
+                public int    TimeUpdated;
+                public int    Visibility;
+                public int    Banned;
+                public string BanReason;
+                public int    Subscriptions;
+                public int    Favorited;
+                public int    LifetimeSubscriptions;
+                public int    LifetimeFavorited;
+                public List<Tag> Tags;
+            }
+        }
+
+        private static HttpClient httpClient = WebRequest.CreateHTTPClient();
+        private static string     baseAPIURL = "https://api.steampowered.com/";
+
+        public static async Task GetWorkshopItemDetails(string[] idList)
+        {
+            string apiModule = "ISteamRemoteStorage/GetPublishedFileDetails/v1/";
+
+            var parameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("itemCount", idList.Length.ToString())
+            };
+
+            for(int i=0; i < idList.Length; i++)
+            {
+                var id = idList[i];
+                parameters.Add(new KeyValuePair<string, string>("publishedfileids["+i.ToString()+"]", id.ToString()));
+            }
+
+            string res = await WebRequest.PostAsync(httpClient, baseAPIURL + apiModule, parameters);
+
+            JToken jsonData = JToken.Parse(res)["response"];
+
+            if((int)jsonData["result"]      == 1
+            && (int)jsonData["resultcount"] == idList.Length)
+            {
+                JArray workshopItems = (JArray)jsonData["publishedfiledetails"];
+
+                foreach(JToken elem in workshopItems)
+                {
+                    Model.WorkshopItemDetails itemDetails = elem.ToObject<Model.WorkshopItemDetails>();
+                }
             }
         }
     }
