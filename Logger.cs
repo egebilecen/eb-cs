ba1b817d930ec4ee974b75c72e10a161b97d6d14
Ege Bilecen <egebilecen@icloud.com>
2022-10-04 11:56:30 +0300

    Update Logger.cs

--- Logger.cs
+++ Logger.cs
@@ -5,23 +5,30 @@ namespace EB_Utility
 {
     public static class Logger
     {
-        public static void Log(string text, string filename = "program.log")
+        public const string LogFile = ".\\program.log";
+        
+        public static string GetLoggingDate()
+        {
+            return DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss") + " UTC";
+        }
+        
+        public static void Log(string text, string filename = null)
         {
             string ex_msg = text +
-                            "\nDate: "+DateTime.Now.ToString("dd/MM/yyyy, HH:mm:ss") +
+                            "\nDate: "+GetLoggingDate() +
                             "\n---------------\n";
-            File.AppendAllText(filename, ex_msg);
+            File.AppendAllText(filename ?? LogFile, ex_msg);
         }
 
-        public static void LogException(Exception ex, string additional_msg="", string filename = "program.log")
+        public static void LogException(Exception ex, string additional_msg="", string filename = null)
         {
             string ex_msg = "Exception: "+ex.GetType().FullName +
                             "\nMessage: "+ex.Message +
                             "\nStack trace: "+ex.StackTrace.Trim() +
-                            "\nDate: "+DateTime.Now.ToString("dd/MM/yyyy, HH:mm:ss") +
+                            "\nDate: "+GetLoggingDate() +
                             (additional_msg != "" ? "\n"+additional_msg : "") +
                             "\n---------------\n";
-            File.AppendAllText(filename, ex_msg);
+            File.AppendAllText(filename ?? LogFile, ex_msg);
         }
     }
 }
