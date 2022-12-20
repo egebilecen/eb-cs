0514167a5b5f6db063e6fcb0f0feeb251565eec6
Ege Bilecen <egebilecen@icloud.com>
2022-12-20 18:54:43 +0300

    Add files via upload

---
+++ ListItem.cs
@@ -0,0 +1,16 @@
+﻿class ListItem
+{
+    public string Text  { get; set; }
+    public object Value { get; set; }
+
+    public ListItem(string text, object obj)
+    {
+        Text  = text;
+        Value = obj;
+    }
+
+    public override string ToString()
+    {
+        return Text;
+    }
+}
