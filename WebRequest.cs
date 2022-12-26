9d08f885a922d4ff21b6b1b276265ba269aeb9eb
Ege Bilecen <egebilecen@icloud.com>
2022-12-26 12:39:25 +0300

    Update WebRequest.cs

--- WebRequest.cs
+++ WebRequest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EB_Utility
