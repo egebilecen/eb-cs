a62d57989e332307b96e1e547aefb6fee00cc08c
Ege Bilecen <egebilecen@icloud.com>
2023-01-01 16:54:09 +0300

    Add files via upload

---
+++ SeleniumChromeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+
+namespace SeleniumBotProgram
+{
+    public static class SeleniumChromeExtensions
+    {
+        public static void WaitUntilElementIsExistXPath(this SeleniumChrome bot, string selector, int timeout = 999999999)
+        {
+            bot.CheckDriver();
+            var wait = new WebDriverWait(bot.driver, TimeSpan.FromSeconds(timeout));
+            wait.IgnoreExceptionTypes(typeof(WebDriverException));
+            wait.Until(
+                ExpectedConditions.ElementExists(By.XPath(selector))
+            );
+        }
+    }
+}
