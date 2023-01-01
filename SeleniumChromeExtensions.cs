fdc2672e801881ddb00f03b20a75d21119f4b9a1
Ege Bilecen <egebilecen@icloud.com>
2023-01-01 16:54:26 +0300

    Update SeleniumChromeExtensions.cs

--- SeleniumChromeExtensions.cs
+++ SeleniumChromeExtensions.cs
@@ -1,20 +1,17 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
 
-namespace SeleniumBotProgram
+public static class SeleniumChromeExtensions
 {
-    public static class SeleniumChromeExtensions
+    public static void WaitUntilElementIsExistXPath(this SeleniumChrome bot, string selector, int timeout = 999999999)
     {
-        public static void WaitUntilElementIsExistXPath(this SeleniumChrome bot, string selector, int timeout = 999999999)
-        {
-            bot.CheckDriver();
-            var wait = new WebDriverWait(bot.driver, TimeSpan.FromSeconds(timeout));
-            wait.IgnoreExceptionTypes(typeof(WebDriverException));
-            wait.Until(
-                ExpectedConditions.ElementExists(By.XPath(selector))
-            );
-        }
+        bot.CheckDriver();
+        var wait = new WebDriverWait(bot.driver, TimeSpan.FromSeconds(timeout));
+        wait.IgnoreExceptionTypes(typeof(WebDriverException));
+        wait.Until(
+            ExpectedConditions.ElementExists(By.XPath(selector))
+        );
     }
 }
