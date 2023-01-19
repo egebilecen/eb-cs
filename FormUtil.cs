50d119be1dcbc83f3137b2527e43d3df2d522e37
Ege Bilecen <egebilecen@icloud.com>
2023-01-19 14:23:16 +0300

    Update FormUtil.cs

--- FormUtil.cs
+++ FormUtil.cs
@@ -9,6 +9,10 @@ namespace EB_Utility
     {
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+
+        // 1 - Green
+        // 2 - Red
+        // 3 - Yellow
         public static void SetState(this ProgressBar pBar, int state)
         {
             SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
