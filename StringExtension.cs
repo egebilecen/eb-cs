ceaa1439fac73639b606901df763d773c1838980
Ege Bilecen <egebilecen@icloud.com>
2023-01-20 15:02:55 +0300

    Update StringExtension.cs

--- StringExtension.cs
+++ StringExtension.cs
@@ -1,4 +1,6 @@
-﻿public static class StringExtension
+using System.Linq;
+
+public static class StringExtensions
 {
     public static string KeyFormat(this string str, params (string, object)[] formatPair)
     {
@@ -10,5 +12,17 @@
         }
 
         return str;
+    }
+
+    public static string FirstCharacterToUpper(this string str)
+    {
+        if(string.IsNullOrEmpty(str)) return str;
+        return str.First().ToString().ToUpper() + str.Substring(1);
+    }
+
+    public static string WordFirstCharacterToUpper(this string str)
+    {
+        if(string.IsNullOrEmpty(str)) return str;
+        return string.Join(" ", str.Split(' ').Select(x => x.FirstCharacterToUpper()));
     }
 }
